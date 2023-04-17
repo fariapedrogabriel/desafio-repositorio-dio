8c64a94dfbcab3fe298f036a5193c298170ae2cd
fariapedrogabriel <pedrogabriel.fv@gmail.com>
2023-04-17 15:26:56 -0300

    Codigo alterado para melhorar o entendimento

--- Cursos/Introdução ao JavaScript/Codigos/Aula 1 - Problema Calculo Custo Viagem.js
+++ Cursos/Introdução ao JavaScript/Codigos/Aula 1 - Problema Calculo Custo Viagem.js
@@ -8,8 +8,8 @@ Você terá 3 variáveis, sendo elas:
 
 //Definindo as Variáveis do Problema;
 const precoCombustivel = Number(5.79);
-const kmPorLitro = Number(30);
-let distanciaPercorrida = Number(100);
+const kmPorLitro = Number(12);
+let distanciaPercorrida = Number(1580);
 
 //Imprimindo os Valores das Variáveis;
 console.log('Preço do Combustível:\n'+ precoCombustivel);
@@ -17,9 +17,12 @@ console.log('Quantos KM o Veíuculo faz por Litro de Combustível:\n'+ kmPorLitr
 console.log('Distância Percorrida em KM:\n' + distanciaPercorrida);
 
 //Calculando e Imprimindo o Valor Final;
-console.log('Valor da Viagem:\n'+ distanciaPercorrida / kmPorLitro * precoCombustivel);
+console.log('Valor da Viagem:\n'+ (distanciaPercorrida / kmPorLitro * precoCombustivel).toFixed(2));
 
 /*
+(Resolvido em 17/04/2023 as 4:00)
 Utilizei do template + para reduzir o numero de linhas ao imprimir cada variavel;
 A ideia foi utilizar apenas 3 variaveis, do contrario, poderia criar mais uma para o resultado;
+(Atualizado em 17/04/2023 as 15:23)
+Adicionado comando ".toFixed(2)" para reduzir e simplificar o número de casas decimais do resultado;
 */
