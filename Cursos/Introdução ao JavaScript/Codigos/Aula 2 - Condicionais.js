fc84d2883f7530335a54cecaf290bcfc4a656cf9
fariapedrogabriel <pedrogabriel.fv@gmail.com>
2023-04-19 14:06:45 -0300

    Estudo de condicionais (Em Andamento)

--- Cursos/Introdução ao JavaScript/Codigos/Aula 2 - Condicionais.js
+++ Cursos/Introdução ao JavaScript/Codigos/Aula 2 - Condicionais.js
@@ -1,4 +1,15 @@
 
 
-console.log("Olá porra");
+const numero = 21;
 
+const numeroPar = (numero) % 2 === 0;
+//poderia adicionar um "console.log("Resultado:\n" + numeroPar);" para imprimir o "resultado", mas deixei oculto para diminuir as informações apresentadas
+
+if (numeroPar) {
+    console.log('Número Par')   
+}  else {
+    console.log('Número Ímpar')
+}    
+// poderia utilizar o "if (!numeroPar)" para imprimir caso seja um número impar, lembrando que ! seria o oposto, porém utilizei o "else" para otimização
+     
+     
