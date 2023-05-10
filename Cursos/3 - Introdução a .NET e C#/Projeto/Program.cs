8e9b9de6b000c6ef6eeb78ecb0045670ad775eac
fariapedrogabriel <pedrogabriel.fv@gmail.com>
2023-05-10 14:12:20 -0300

    Estudo sobre .net e c#

---
+++ Cursos/3 - Introdução a .NET e C#/Projeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Pedro";
+pessoa1.Idade = 31;
+pessoa1.Apresentar();
+
+Pessoa pessoa2 = new Pessoa();
+
+pessoa2.Nome = "Gabriel";
+pessoa2.Idade = 30;
+pessoa2.Apresentar();
